390e47d1fa9e5d10e1da3e3e1b6b3065cbfe23e5
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-05-19 21:17:03 -0400

    main: WIP system with two simple tasks ping-ponging themselves

--- src/main.c
+++ src/main.c
@@ -16,37 +16,63 @@
 
 #include <kernel.h>
 
-#include <ucontext.h>
-#include <unistd.h>
-
-/**
- * Sample task function from page 80
- */
-#define task_ID 1
 void task(VP_INT exinf) {
 	/* Body of the task */
+	DBG("Hello, World!");
+	act_tsk(2);
 	ext_tsk();
+	PANIC("End of sample task reached...", E_SYS);
 }
 
+void other_task(VP_INT exinf) {
+	static INT s_i = 0;
+	INT i = 0;
+	i++;
+	s_i++;
+	DBG("This should stay at 1: %08d", i);
+	DBG("This should increment: %08d", s_i);
+	if (i != 1) {
+		PANIC("Stack-allocated variables are not working properly...", E_SYS);
+	}
+	if (s_i <= 10) {
+		act_tsk(1);
+	}
+}
+
+static T_CTSK task_info = {0};
+static T_CTSK other_info = {0};
+
 int main(int argc, char** argv) {
+	ER_ID ret;
+
 	zepto_init();
 
 	// This would be in the application-specific initialization normally...
-	{
-		DBG("Starting up...");
-		DBG("... creating task");
+	// For now, implemented as `main`...
+	DBG("Starting up...");
 
-		ER_ID ret;
+	DBG("... creating task");
+	task_info.task = (FP)task;
+	task_info.exinf = NULL;
 
-		T_CTSK task_info = {0};
-		task_info.task = (FP)task;
-
-		ret = cre_tsk(task_ID, &task_info);
-		if (ret < 0) {
-			PANIC("Could not create task...", ret);
-		}
-		iact_tsk(task_ID);
+	ret = cre_tsk(1, &task_info);
+	if (ret < 0) {
+		PANIC("Could not create task...", ret);
 	}
 
+	DBG("... creating other_task");
+	other_info.task = (FP)other_task;
+	other_info.exinf = NULL;
+	other_info.tskatr |= TA_ACT; // Start active
+
+	ret = cre_tsk(2, &other_info);
+	if (ret < 0) {
+		PANIC("Could not create other_task...", ret);
+	}
+
+	// Launch
 	zepto_run();
+
+	// This should not happen
+	PANIC("End of main() reached!", E_SYS);
 }
